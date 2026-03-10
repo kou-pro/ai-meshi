b4df3e2c0c58da6812861af2bb00696f1770f8dc
kou-pro <kohei.yama.11@gmail.com>
2026-03-10 11:59:45 +0900

    style: users_controller インデント修正

--- rails/app/controllers/api/v1/users_controller.rb
+++ rails/app/controllers/api/v1/users_controller.rb
@@ -11,23 +11,24 @@ module Api
       end
 
       def recipes
-  user = User.find(params[:id])
-  recipes = user.recipes.where(is_published: true).order(created_at: :desc)
-  render json: {
-    user: {
-      id: user.id,
-      name: user.name
-    },
-    recipes: recipes.map { |r|
-      {
-        id: r.id,
-        title: r.title,
-        content: r.content,
-        created_at: r.created_at
-      }
-    }
-  }
-end
+        user = User.find(params[:id])
+        recipes = user.recipes.where(is_published: true).order(created_at: :desc)
+        render json: {
+          user: {
+            id: user.id,
+            name: user.name
+          },
+          recipes: recipes.map { |r|
+            {
+              id: r.id,
+              title: r.title,
+              content: r.content,
+              created_at: r.created_at
+            }
+          }
+        }
+      end
+      
     end
   end
 end
